3fc168f1dfcb1a68c9ee6a765fef66e04ece6839
suvarnamahajan <suvarnaavinashmahajan@gmail.com>
2017-06-12 13:45:34 +0530

    updated num

---
+++ rubyforbeginner_num.rb
@@ -0,0 +1,16 @@
+puts "How many hours are in a year."
+puts "hours",365*24
+puts "How many minutes are in a decade?"
+puts "minutes are",10*365*24*60
+puts "How many seconds old are you?"
+puts "seconds",23*365*24*60*60
+puts 3.0 / 2
+puts 3 / 2.0
+puts 4 ** 2.0
+puts 4.1 % 2
+arr=[0, 1, 2, 99, -502]
+arr1=arr.select{|n|n%2==0}
+arr2=arr.select{|n|n%2==1}
+puts "even values are",arr1
+puts "odd values are",arr2
+puts "hello".length + "world".length
